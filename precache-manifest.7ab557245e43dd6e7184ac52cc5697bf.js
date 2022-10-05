c5afbc79c770533c1ba0801082ab5af701e6cd8f
SoloPane <me@solopane.me>
2022-10-06 03:05:44 +0600

    update

---
+++ precache-manifest.7ab557245e43dd6e7184ac52cc5697bf.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f9a51a6c699b9550d51dc828389e68",
+    "url": "https://hotswap.link/index.html"
+  },
+  {
+    "revision": "9ea7ec4ccc0859fa6281",
+    "url": "https://hotswap.link/static/css/main.2959e852.chunk.css"
+  },
+  {
+    "revision": "ba52d530c6227bc26b73",
+    "url": "https://hotswap.link/static/js/0.621b825a.chunk.js"
+  },
+  {
+    "revision": "a08ff9df48815707d7e6",
+    "url": "https://hotswap.link/static/js/10.f9d469e8.chunk.js"
+  },
+  {
+    "revision": "b99237e7a46ddf3784ac",
+    "url": "https://hotswap.link/static/js/3.8f620a35.chunk.js"
+  },
+  {
+    "revision": "6e34345e5a3e64b91947dfdc3e2a461a",
+    "url": "https://hotswap.link/static/js/3.8f620a35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d046f5ce87642a3b560a",
+    "url": "https://hotswap.link/static/js/4.ac6339cf.chunk.js"
+  },
+  {
+    "revision": "ede94d656dbca1ff3953",
+    "url": "https://hotswap.link/static/js/5.d298ae82.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "https://hotswap.link/static/js/5.d298ae82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117b25c8c7e43783cbc9",
+    "url": "https://hotswap.link/static/js/6.eb5e1100.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://hotswap.link/static/js/6.eb5e1100.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4560b06c07bff8cbc863",
+    "url": "https://hotswap.link/static/js/7.a45a0a15.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://hotswap.link/static/js/7.a45a0a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48b6274fcde41f5cb242",
+    "url": "https://hotswap.link/static/js/8.15a67b0e.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://hotswap.link/static/js/8.15a67b0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6eb4afbd24800081d08",
+    "url": "https://hotswap.link/static/js/9.a59911fd.chunk.js"
+  },
+  {
+    "revision": "9ea7ec4ccc0859fa6281",
+    "url": "https://hotswap.link/static/js/main.28e0912c.chunk.js"
+  },
+  {
+    "revision": "f904a37a6acff7d05934",
+    "url": "https://hotswap.link/static/js/runtime-main.c1c20bfa.js"
+  },
+  {
+    "revision": "562acd6fbd02e78fb07b839c5c5c0c3e",
+    "url": "https://hotswap.link/static/media/hotdog-van.562acd6f.svg"
+  },
+  {
+    "revision": "42ec72d066bec105d8c4a2e002884c9e",
+    "url": "https://hotswap.link/static/media/hots-supply.42ec72d0.svg"
+  },
+  {
+    "revision": "915f7a38df57313501d5acfacd0fd99b",
+    "url": "https://hotswap.link/static/media/hots.915f7a38.svg"
+  },
+  {
+    "revision": "c1ee022259e70a79be25066091ce16fa",
+    "url": "https://hotswap.link/static/media/logo.c1ee0222.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "https://hotswap.link/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "37f2bc6ecee2eb29b3cf6799ce58bf58",
+    "url": "https://hotswap.link/static/media/wallet-connect.37f2bc6e.svg"
+  }
+]);
